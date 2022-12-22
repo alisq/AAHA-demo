15c3dc74e37112824fe45cd4b1991887735678b5
alisq <ali.shamas.qadeer@gmail.com>
2022-12-22 14:46:49 -0500

    present.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ $("#menu-button").click(function(){
     $(this).toggleClass("active")
     $("#menu").toggleClass("active")
 })
+
+
+$("#menu li").click(function(){
+    id= "#"+$(this).attr("id").replace("menu-","");
+    $("#menu").fadeOut(400);
+    setTimeout(function(){
+        $("#menu").removeClass("active");
+        $(document).scrollTo(id, 300)
+    },400)
+})
